9d9024845aead2f24f43ec6f7e5425db4fc69c15
vilhota30 <111867115+vilhota30@users.noreply.github.com>
2024-04-02 19:14:23 +0300

    add homework3

---
+++ homework3/code/index3.js
@@ -0,0 +1,29 @@
+const stringData = 'text example';
+console.log(typeof stringData);
+
+const numberData = 36;
+console.log(typeof numberData);
+
+const booleanData = true;
+console.log(booleanData);
+
+let nullData = null;
+console.log(typeof nullData);
+// typeof - [], null, Object, Math => object 
+
+let undefinedData;
+console.log(typeof undefinedData);
+
+const UserObjectData = {};
+console.log(typeof UserObjectData);
+
+const bigIntData = 8n;
+console.log(typeof bigIntData);
+
+const symbolData = Symbol("foo");
+console.log(symbolData);
+
+
+
+
+
